2ea7c1904201ae84167fe8239a9c41562a25c467
qodirovoybekjon4929 <qodirovoybekjon08@gmail.com>
2023-03-03 00:20:55 +0500

    next-app

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,9 +1,24 @@
 // imports
+import router from 'next/router'
 
+// methods
+function goToHomePage() {
+    router.push('/')
+}
+
+// page
 const NotFoundPage = () => {
     return (
         <div className='pageName'>
-            404 PAGE
+            <div>
+                <div>
+                    404 PAGE
+                </div>
+
+                <div className="back-to-home-button" onClick={() => goToHomePage()} style={{marginTop: '10px'}}>
+                    Back to home
+                </div>
+            </div>
         </div>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,21 @@
+// imports
+import router from 'next/router'
+
+// methods
+function goToPostsPage() {
+  router.push('/posts-page')
+}
+
+// posts
 export default function Home() {
   return (
     <>
       <div className="pageName">
-        Home page
+        <div>
+          Home page
+          <div className="go-to-posts-button" style={{marginTop: '10px'}} onClick={() => goToPostsPage()}>Posts</div>
+        </div>
       </div>
     </>
-  )
+  );
 }
